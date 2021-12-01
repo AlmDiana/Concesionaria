40a18df0bf6c41c78c159cff5e20197d0f4a9c62
Diana Almeida <58041267+AlmDiana@users.noreply.github.com>
2021-12-01 14:18:49 -0500

    Create home.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.main')
+
+@section('title','Inicio')
+
+@section('content')
+<h1>Bienvenidos - CONCESIONARIA VENGADORES</h1>
+<hr>
+
+<?php 
+echo "<a href=\"https://drivetribe.imgix.net/P5F9dSZ-TqOhLpsXotLJ5g?w=1920&h=1080&auto=format,compress&fit=crop&crop=faces&max-w=1280&max-h=720\">
+<img src=\"https://drivetribe.imgix.net/P5F9dSZ-TqOhLpsXotLJ5g?w=1920&h=1080&auto=format,compress&fit=crop&crop=faces&max-w=1280&max-h=720\"></a>";
+?>
+
+@endsection()
